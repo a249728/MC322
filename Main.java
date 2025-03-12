c058c03e74d135f4ead0406e3935cd7fa9804c9c
Ana Luiza <a249728@dac.unicamp.br>
2025-03-12 14:03:37 -0300

    Cria método da main que executa métodos da classe Robô

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
-public class Main{
+public class Main {
 
-    public static void main(){
-        
+    public static void main(String[] args) {
+        Robo C3PO = new Robo("C3PO", 0, 0);
+        Robo R2D2 = new Robo("R2D2", 0, 0);
+
+        C3PO.mover(3, 0);
+        R2D2.mover(2, 2);
+
+        System.out.println(C3PO.exibirPosicao());
+        System.out.println(R2D2.exibirPosicao());
     }
 }
