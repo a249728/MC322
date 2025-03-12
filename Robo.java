79e284843be12c35801c2fd5a9279a2c6763346b
Ana Luiza <a249728@dac.unicamp.br>
2025-03-12 13:36:22 -0300

    Adiciona arquivos e implementa classe Robo

--- Robo.java
+++ Robo.java
@@ -1,4 +1,21 @@
-public class Robo{
+public class Robo {
 
+    private String nome;
+    private int posicaoX;
+    private int posicaoY;
+
+    public Robo(String n, int x, int y) {
+        this.nome = n;
+        this.posicaoX = x;
+        this.posicaoY = y;
+    }
+
+    public void mover(int deltaX, int deltaY) {
+        this.posicaoX += deltaX;
+        this.posicaoY += deltaY;
+    }
+
+    public String exibirPosicao(){
+        return this.posicaoX + ", " + this.posicaoY;
+    }
 }
-
