99e08193faae05c4fb316364744de1bb3a3c1daf
LeonerdCdL <leonardocluca@gmail.com>
2025-03-14 13:20:04 -0300

    Ambiente feito

--- Lab01/Ambiente.java
+++ Lab01/Ambiente.java
@@ -1,3 +1,16 @@
 public class Ambiente{
-    
+    private int largura;
+    private int altura;
+
+    public Ambiente(int l, int a){
+        this.largura=l;
+        this.altura=a;
+    }
+
+    public boolean dentroDosLimites(int x, int y){
+        if(x<this.largura && y<this.altura){
+            return True;
+        }
+        return False;
+    }
 }
