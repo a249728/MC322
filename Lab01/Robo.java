03edf97a57fce3561419a5ceab7c1cbfc9f14440
Ana Luiza <a249728@dac.unicamp.br>
2025-03-15 15:15:55 -0300

    Adiciona comentários

--- Lab01/Robo.java
+++ Lab01/Robo.java
@@ -1,24 +1,29 @@
 public class Robo {
+    // Declara as propriedades da classe Robo
     private String nome;
     private int posicaoX;
     private int posicaoY;
 
     public Robo(String n, int x, int y) {
+        // Metodo construtor da classe
         this.nome = n;
         this.posicaoX = x;
         this.posicaoY = y;
     }
 
     public void mover(int deltaX, int deltaY) {
+        // Atualiza as coordenadas do robo adicionando a variacao de posicao desejada
         this.posicaoX += deltaX;
         this.posicaoY += deltaY;
     }
 
     public int[] exibirPosicao() {
+        // Retorna o vetor de coordenadas do robo
         return new int[] { this.posicaoX, this.posicaoY };
     }
 
     public String retornarNome() {
+        // Retorna o nome do robo
         return this.nome;
     }
 }
