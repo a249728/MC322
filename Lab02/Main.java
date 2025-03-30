72d2a785204457655377bb013c0bffeff8065d09
Ana Luiza <a249728@dac.unicamp.br>
2025-03-30 20:35:38 -0300

    Altera main para adicionar os novos metodos e robos criados

--- Lab02/Main.java
+++ Lab02/Main.java
@@ -1,36 +1,59 @@
 public class Main {
         public static void main(String[] args) {
             // Cria um ambiente
-            Ambiente Ilha_Base = new Ambiente(500, 500, 500);
+            Ambiente ilhaMecanimais = new Ambiente(500, 500, 500);
             System.out.println("Criando o ambiente Ilha Base de dimensões 500x500x500");
 
-            // Cria robos e adiciona-os a lista de robos ativos no ambiente criado
-            Robo Rex = new Robo("Rex", 500, 500, "Norte");
-            RoboTerrestre Sasquatch = new RoboTerrestre("Sasquatch", 0, 0, "Norte", 100);
-            RoboAereo Unicornio = new RoboAereo("Unicornio", 0, 0, "Norte", 0, 100);
-            Ilha_Base.adicionarRobo(Rex);
-            Ilha_Base.adicionarRobo(Sasquatch);
-            Ilha_Base.adicionarRobo(Unicornio);
+            // Cria um robo criador que cria o restante dos robos
+            RoboCriador JeffRosen = new RoboCriador("JeffRosen", 0, 0, "Leste", 1, 499);    
+            Robo Sasquatch = JeffRosen.criarRobo(ilhaMecanimais, "Sasquatch", "Leste");
+            moverRoboAereo(JeffRosen, 0, 1, 0, ilhaMecanimais);
+            RoboTerrestre Rex = JeffRosen.criarRoboTerrestre(ilhaMecanimais, "Rex", "Leste", 100);
+            moverRoboAereo(JeffRosen, 0, 1, 0, ilhaMecanimais);
+            RoboAereo Unicornio = JeffRosen.criarRoboAereo(ilhaMecanimais, "Unicornio", "Leste", 0, 99);
+            moverRoboAereo(JeffRosen, 0, 1, 0, ilhaMecanimais);
+            RoboSubterraneo Corujao = JeffRosen.criarRoboSubterraneo(ilhaMecanimais, "Corujao", "Leste", 0, -99);
+            moverRoboAereo(JeffRosen, 0, 1, 0, ilhaMecanimais);
+            RoboLaser Komodo = JeffRosen.criarRoboLaser(ilhaMecanimais, "Komodo", "Leste", 100, 10);
+            moverRoboAereo(JeffRosen, 0, 1, 0, ilhaMecanimais);
+            RoboCorredor Mouse = JeffRosen.criarRoboCorredor(ilhaMecanimais, "Mouse", "Leste", 200, 100);
 
             // Exibe a posicao inicial dos robos criados
-            imprimirPosicao(Rex);
             imprimirPosicao(Sasquatch);
+            imprimirPosicao(Rex);
+            imprimirPosicao(Mouse);
+            imprimirPosicao(Komodo);
             imprimirPosicaoAereo(Unicornio);
+            imprimirPosicaoAereo(Corujao);
+            imprimirPosicaoAereo(JeffRosen);
+            
     
             // Checa se os robos estão inicialmente dentro dos limites do ambiente
-            checarLimites(Ilha_Base, Rex);
-            checarLimites(Ilha_Base, Sasquatch);
-            checarLimitesAereo(Ilha_Base, Unicornio);
+            checarLimites(ilhaMecanimais, Sasquatch);
+            checarLimites(ilhaMecanimais, Rex);
+            checarLimites(ilhaMecanimais, Mouse);
+            checarLimites(ilhaMecanimais, Komodo);
+            checarLimitesAereo(ilhaMecanimais, Unicornio);
+            checarLimitesAereo(ilhaMecanimais, Corujao);
+            checarLimitesAereo(ilhaMecanimais, JeffRosen);
     
-            // Move os robos criados
-            moverRobo(Rex, -3, 0, Ilha_Base);
-            moverRoboTerrestre(Sasquatch, 400, 0, Ilha_Base);
-            moverRoboAereo(Unicornio, 400, 0, 100, Ilha_Base);
+            // Move alguns dos robos criados
+            moverRobo(Sasquatch, 501, 0, ilhaMecanimais);
+            moverRoboTerrestre(Rex, 10, 0, ilhaMecanimais);
+            moverRoboAereo(Unicornio, 10, 0, 0, ilhaMecanimais);
+            moverRoboCorredor(Mouse, 400, ilhaMecanimais);
+            moverRobo
+
+
+            //Utiliza dos métodos (ainda não utilizados) específicos dos robos especiais
+            Komodo
+            
     
             // Exibe a posicao final dos robos criados
             imprimirPosicao(Rex);
             imprimirPosicao(Sasquatch);
             imprimirPosicaoAereo(Unicornio);
+
     }
 
     public static void moverRobo(Robo robo, int dx, int dy, Ambiente amb) {
@@ -49,6 +72,29 @@ public class Main {
         if (robo.mover(dx, dy, amb)) {
             int coord[] = robo.exibirPosicao();
             System.out.println("O robo " + robo.retornarNome() + " moveu para a posicao: (" + coord[0] + ", " + coord[1] + ")");
+        }
+        else {
+            System.out.println("O robo " + robo.retornarNome() + " não se moveu");
+        }
+    }
+
+    public static void moverRoboCorredor(RoboCorredor robo, int d, Ambiente amb) {
+        // Utiliza do metodo mover para atualizar as posicoes do robo terrestre e imprime as novas coordenadas adquiridas
+        if (robo.mover(d, amb)) {
+            int coord[] = robo.exibirPosicao();
+            System.out.println("O robo " + robo.retornarNome() + " moveu para a posicao: (" + coord[0] + ", " + coord[1] + ")");
+        }
+        else {
+            System.out.println("O robo " + robo.retornarNome() + " não se moveu");
+        }
+    }
+
+    public static void moverRoboSubterraneo(RoboSubterraneo robo, int dx, int dy, int dz, Ambiente amb) {
+        // Utiliza do metodo mover para atualizar as posicoes do robo terrestre e imprime as novas coordenadas adquiridas
+        if (robo.mover(dx, dy, dz, amb)) {
+            int coord_xy[] = robo.exibirPosicao();
+            int z = robo.exibirAltura();
+            System.out.println("O robo " + robo.retornarNome() + " moveu para a posicao: (" + coord_xy[0] + ", " + coord_xy[1] + ", " + z + ")");
         }
         else {
             System.out.println("O robo " + robo.retornarNome() + " não se moveu");
