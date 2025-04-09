46624aef75d823eb65cba6f8bae0348e2908f00c
Ana Luiza <a249728@dac.unicamp.br>
2025-04-09 18:34:16 -0300

    Adiciona arquivo obstaculos ja aplicando ENUM

---
+++ src/Lab03/Obstaculo.java
@@ -0,0 +1,28 @@
+public enum Obstaculo {
+    PEDRA (3, 3, 3),
+    ARVORE (1, 1, 10),
+    BURACO (5, 5, -5),
+    LAGO (21, 21, -8);
+
+    private final int comprimento;  
+    private final int largura;
+    private final int altura;
+
+    Obstaculo(int c, int l, int a) {
+        this.comprimento = c;
+        this.largura = l;
+        this.altura = a;
+    }
+
+    public int getComprimento() {
+        return comprimento; 
+    }
+
+    public int getLargura() {
+        return largura; 
+    }
+
+    public int getAltura() {
+        return altura; 
+    }
+}
---
+++ src/Lab03/Sensor.java
@@ -0,0 +1,3 @@
+public class Sensor {
+    private double raioDeAlcance;
+}
