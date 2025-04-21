d9c38edaa622b8aefac543832c3515e02c4c15a6
Ana Luiza <a249728@dac.unicamp.br>
2025-04-21 01:00:06 -0300

    Altera sensor colisao

--- src/Lab03/SensorColisao.java
+++ src/Lab03/SensorColisao.java
@@ -11,13 +11,10 @@ public class SensorColisao extends Sensor {
         }
         ArrayList<Robo> robos = amb.retornarRobosAtivos();
         ArrayList<Obstaculo> obstaculos = amb.retornarObstaculos();
-        int coord[] = robo.exibirPosicao();
-        int deltaX = x - coord[0];
-        int deltaY = y - coord[1];
         int altura1 = 0;
         int altura2 = 0;
         for (Robo rob : robos) {
-            if (rob.exibirPosicao()[0] == coord[0] + deltaX && rob.exibirPosicao()[1] == coord[1] + deltaY && rob != robo) {
+            if (rob.exibirPosicao()[0] == x && rob.exibirPosicao()[1] == y && rob != robo) {
                 if (rob.getClass() == RoboAereo.class) {
                     // converter rob do tipo Robo para RoboAereo e adicionar a altura2 o valor de sua altura
                     RoboAereo roboAereo = (RoboAereo) rob;
@@ -36,7 +33,7 @@ public class SensorColisao extends Sensor {
         for (Obstaculo obstaculo : obstaculos) {
             int alturaObstaculo = obstaculo.getObstaculo().getAltura();
             int alturaRobo = 0;
-            if (obstaculo.getPosicaoX() == coord[0] && y > obstaculo.getPosicaoY() && y < obstaculo.getPosicaoY() + obstaculo.getObstaculo().getLargura()) {
+            if (obstaculo.getPosicaoX() == x && y > obstaculo.getPosicaoY() && y < obstaculo.getPosicaoY() + obstaculo.getObstaculo().getLargura()) {
                 if (robo.getClass() == RoboAereo.class) {
                     // converter robo do tipo Robo para RoboAereo e adicionar a alturaRobo o valor de sua altura
                     RoboAereo roboAereo = (RoboAereo) robo;
