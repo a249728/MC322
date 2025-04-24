9f584332e224f55a69b1d3b0d2612742d0dd53a0
LeonerdCdL <leonardocluca@gmail.com>
2025-04-24 00:12:13 -0300

    SensorPressao modificado

--- src/Lab03/SensorPressao.java
+++ src/Lab03/SensorPressao.java
@@ -20,15 +20,15 @@ public class SensorPressao extends Sensor {
         final double PRESSAO_NIVEL_MAR = 1.0; // em atm
 
         // Fator de variação da pressão por unidade de altura
-        final double FATOR_ALTURA = 0.1; // diminui 0.1 atm por unidade acima do nível do mar
-        final double FATOR_PROFUNDIDADE = 0.2; // aumenta 0.2 atm por unidade abaixo do nível do mar
+        final double FATOR_ALTURA = 0.00012514185; // diminui rho*g Pascal, ou rho*g/101325=1.293*9.80665/101325=0.00012514185 atm por unidade acima do nível do mar
+        final double FATOR_PROFUNDIDADE = 0.5337643696; // aumenta rho*g Pascal, ou rho*g/101325=5515*9.80665/101325=0.5337643696 atm por unidade abaixo do nível do mar
 
         if (z >= 0) {
             // Quanto mais alto, menor a pressão
             return Math.max(0, PRESSAO_NIVEL_MAR - (z * FATOR_ALTURA));
         } else {
             // Quanto mais baixo, maior a pressão
-            return PRESSAO_NIVEL_MAR + (Math.abs(z) * FATOR_PROFUNDIDADE);
+            return PRESSAO_NIVEL_MAR + ((-z) * FATOR_PROFUNDIDADE);
         }
     }
 }
